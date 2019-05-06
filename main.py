f739a0fdee750c3afe1d648aae8b362d3d954a6e
Mike <mike.pratt@trelleborg.com>
2019-05-06 21:26:38 +0100

    Added method for getting gameweek data.
    Also added check to see if the user participated in the specified gameweek number.

--- main.py
+++ main.py
@@ -12,19 +12,13 @@ async def main():
     fpl_session = FplSession(secrets.email, secrets.password)
     await get_classic_league(fpl_session, 152458)
     await fpl_session.close()
+    create_gameweek_table(1)
+
     # print(jsonpickle.encode(user_gameweek_histories))
     # file = open("gameweeks.txt", "w")
     # file.write(jsonpickle.encode(user_gameweek_histories))
     # file.close
     print('closed')
-
-
-async def get_my_team(fpl_session, id):
-    async with fpl_session.session:
-        await fpl_session.login()
-        user = await fpl_session.fpl.get_user(id)
-        team = await user.get_team()
-    print(team)
 
 
 async def get_classic_league(fpl_session, league_id):
@@ -40,7 +34,6 @@ async def get_classic_league(fpl_session, league_id):
             users_gameweeks = await get_user_gameweek_history(fpl_session, user.id)
             # key_pair = {'user': user, 'gameweeks': users_gameweeks}
             user.set_game_weeks(users_gameweeks)
-            print(user.get_game_weeks())
 
 
 async def get_user_gameweek_history(fpl_session, user_id):
@@ -51,6 +44,20 @@ async def get_user_gameweek_history(fpl_session, user_id):
         user_gameweek = Gameweek(gameweek['event'], user_id, gameweek['points'], gameweek['total_points'], 0)
         gameweeks.append(user_gameweek)
     return gameweeks
+
+
+def create_gameweek_table(gameweek_number):
+    gameweek_table = []
+    for user in users:
+        user_gameweek = None
+        for gameweek in user.get_game_weeks():
+            if gameweek.week == gameweek_number:
+                user_gameweek = Gameweek(gameweek.week, gameweek.user_id, gameweek.gameweek_points, gameweek.total_points, gameweek.rank)
+
+        if user_gameweek == None: # user may not have participated in this game week
+            continue
+        gameweek_data = {'user': user.user_name, 'team_name': user.team_name, 'gameweek_points': user_gameweek.gameweek_points, 'total_points': user_gameweek.total_points, 'rank': user_gameweek.rank}
+        print(gameweek_data)
 
 
 asyncio.run(main())
