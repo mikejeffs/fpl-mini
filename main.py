105e25b6b42523c30401f6f25c4686ab43780a09
Mike <mike.pratt@trelleborg.com>
2019-05-06 19:12:37 +0100

    Fixed bug where you could not get gameweek history for more than one user.
    I was calling 'async with fpl_session.session:' each time in the get_user_gameweek_history method, which caused the application to hang after fetching data for the first user.

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ async def main():
     fpl_session = FplSession(secrets.email, secrets.password)
     await get_classic_league(fpl_session, 152458)
     await fpl_session.close()
+    file = open("gameweeks.txt", "w")
+    file.write(jsonpickle.encode(user_gameweek_histories))
+    file.close
+    print('closed')
 
 
 async def get_my_team(fpl_session, id):
@@ -29,22 +33,27 @@ async def get_classic_league(fpl_session, league_id):
         for standing_entry in standings['results']:
             user = User(standing_entry['entry'], standing_entry['entry_name'], standing_entry['player_name'])
             users.append(user)
-        # await get_user_gameweek_history(fpl_session, users[0].id)
+
         for user in users:
-        #     print(jsonpickle.encode(user))
+            print('the perils of Python ---------_________-----------------')
+            print(jsonpickle.encode(user))
             await get_user_gameweek_history(fpl_session, user.id)
+            print('-------- Get history for next user --------')
 
 
 async def get_user_gameweek_history(fpl_session, user_id):
-    async with fpl_session.session:
-        await fpl_session.login()
-        # TODO: Replace user_id with user, standing_entry needs to be converted to a User object.
-        user = await fpl_session.fpl.get_user(user_id)
-        # print(jsonpickle.encode(user))
-        history = await user.get_gameweek_history()
-        print('---------------')
-        print(history)
-        user_gameweek_histories.append(history)
+    # async with fpl_session.session:
+    print('1')
+    # await fpl_session.login()
+    # TODO: Replace user_id with user, standing_entry needs to be converted to a User object.
+    user = await fpl_session.fpl.get_user(user_id)
+    print('2')
+    # print(jsonpickle.encode(user))
+    history = await user.get_gameweek_history()
+    print('3')
+    print('---------------')
+    print(history)
+    user_gameweek_histories.append(history)
     print(user_gameweek_histories)
 
 
